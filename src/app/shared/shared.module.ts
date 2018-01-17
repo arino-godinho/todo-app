abf78a361ee010663b1b503d19cc71364d1672e2
Arino <Arino@IBS.local>
2018-01-17 18:13:00 -0200

    layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,26 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
-import { MatCheckboxModule, MatToolbarModule } from '@angular/material';
+import {
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+} from '@angular/material';
 
 const modules = [
   CommonModule,
   FormsModule,
   MatToolbarModule,
   MatCheckboxModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatIconModule,
   FlexLayoutModule
 ];
 
